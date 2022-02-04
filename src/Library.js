521ff0fca1424acee00ea037f7619be672b906e2
Josh Mallery <josh.mallery@gmail.com>
2022-02-03 23:13:52 -0700

    Add partial Library

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,30 @@
 
+function createLibrary(libName) {
+var library = {}
+  library.name = libName
+  library.shelves = {
+    fantasy: [],
+    fiction: [],
+    nonFiction: []
+  }
+ return library
+}
+
+function addBook(libName1, bookTitle) {
+for (var genre in libName1.shelves){
+  if (bookTitle.genre === genre){
+    libName1.shelves[genre].push(bookTitle)
+    }
+  }
+  return libName1;
+}
+
+function checkoutBook() {
+  return''
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+   createLibrary,
+   addBook,
+  checkoutBook
 };
