9f3c4ef4db63a6b698ebf7f199f961f5076263a3
Josh Mallery <josh.mallery@gmail.com>
2022-02-08 18:02:29 -0700

    Refactor Library

--- src/Library.js
+++ src/Library.js
@@ -10,27 +10,45 @@ var library = {}
  return library
 }
 
-function addBook(libName1, bookTitle) {
-for (var genre in libName1.shelves){
-  if (bookTitle.genre === genre){
-    libName1.shelves[genre].push(bookTitle)
-    }
-  }
-  return libName1;
+// function addBook(library, book) {
+// for (var genre in library.shelves){
+//   if (book.genre === genre){
+//     library.shelves[genre].push(book)
+//     }
+//   }
+//   return library;
+// }
+
+function addBook(library, book) {
+library.shelves[book.genre].push(book)
 }
 
-function checkoutBook(dLibrary, bookTitle, genre) {
-for (var genres in dLibrary.shelves){ // loop through the keys of shelves
-  if ( genres === genre ){  // if the genre of the bookTitle is equal to the shelf name
-     for (var i = 0; i < dLibrary.shelves[genres].length; i++){
-       if (dLibrary.shelves[genres][i].title === bookTitle) {
-           dLibrary.shelves[genres].splice([genres],1)  // remove the book
-              return `You have now checked out ${bookTitle} from the ${dLibrary.name}` // inform of book removal
-        }
-      }
-    }
-  } return `Sorry, there are currently no copies of ${bookTitle} available at the ${dLibrary.name}` // else say there are no books
+
+function checkoutBook(library, book, genre) {
+   if(!library.shelves[genre].length){
+      return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
+  }
+ library.shelves[genre].splice(0,1)
+return `You have now checked out ${book} from the ${library.name}`
+
+// function checkoutBook(library, book, genre) {
+// library.shelves[genre].splice(0,1)
+// return `You have now checked out ${book} from the ${library.name}`
+
+
 }
+// function checkoutBook(dLibrary, bookTitle, genre) {
+// for (var genres in dLibrary.shelves){ // loop through the keys of shelves
+//   if ( genres === genre ){  // if the genre of the bookTitle is equal to the shelf name
+//      for (var i = 0; i < dLibrary.shelves[genres].length; i++){
+//        if (dLibrary.shelves[genres][i].title === bookTitle) {
+//            dLibrary.shelves[genres].splice([genres],1)  // remove the book
+//               return `You have now checked out ${bookTitle} from the ${dLibrary.name}` // inform of book removal
+//         }
+//       }
+//     }
+//   } return `Sorry, there are currently no copies of ${bookTitle} available at the ${dLibrary.name}` // else say there are no books
+// }
 module.exports = {
    createLibrary,
    addBook,
